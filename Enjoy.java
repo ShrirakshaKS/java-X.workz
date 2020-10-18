1d51b45f3dd5dfc5688d4b24211875f71121204c
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-18 19:04:23 +0530

    Add files via upload

---
+++ Enjoy.java
@@ -0,0 +1,17 @@
+class Enjoy
+{
+public static void main(String[] params)
+{
+System.out.println("Lets have fun");
+music();
+}
+public static void music()
+{
+System.out.println("Music refreshes mind");
+drinking();
+}
+public static void drinking()
+{
+System.out.println("Cold drinks");
+}
+}
