dab378f6d45e592fb55b08f262eb56416b978a33
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-18 20:05:49 +0530

    Add files via upload

---
+++ Write.java
@@ -0,0 +1,17 @@
+class Write
+{
+public static void main(String[] args)
+{
+System.out.println("Handwritten script");
+
+write();
+write();
+write();
+}
+
+public static void write()
+{
+System.out.println("Writing is very close to heart");
+}
+
+}
