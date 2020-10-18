fea6f2146809d6d0d4c17b2f0ada1565985ffcd8
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-18 19:03:23 +0530

    Add files via upload

---
+++ Party.java
@@ -0,0 +1,32 @@
+class Party
+{
+
+public static void main(String[] params)
+{
+System.out.println("Lets begin the party!!....");
+food();
+entertainment();
+disco();
+exit();
+}
+
+public static void food()
+{
+System.out.println("Welcome to the food section.");
+}
+
+public static void entertainment()
+{
+System.out.println("lets enjoy the moment");
+}
+
+public static void disco()
+{
+System.out.println("Step on to stage, lets dance...");
+}
+
+public static void exit()
+{
+System.out.println("Thanks for being there!!!!!");
+}
+}
