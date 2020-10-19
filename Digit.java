66655fafe4025c0f2ac4d4f9756e4cb1064685e0
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-19 22:33:29 +0530

    Add files via upload

---
+++ Digit.java
@@ -0,0 +1,22 @@
+public class Digit
+{
+public static void main(String[] keys)
+{
+byte daysInAWeek=7;
+short electricityBillPerMonth=340;
+int salaryOfPresidentOfIndia=50000;
+long lightyear=600000;
+float massValueOfIron=55.845f;
+double squarerootOf14=3.741657386d;
+char f=102;
+boolean bloodColorIsRed= true;
+System.out.println(daysInAWeek);
+System.out.println(electricityBillPerMonth);
+System.out.println(salaryOfPresidentOfIndia);
+System.out.println(lightyear);
+System.out.println(massValueOfIron);
+System.out.println(squarerootOf14);
+System.out.println(f);
+System.out.println(bloodColorIsRed);
+}
+}
---
+++ Fact.java
@@ -0,0 +1,22 @@
+public class Fact
+{
+public static void main(String[] real)
+{
+byte digitInAClockDisplay=12;
+short priceOfGoldPerGram=3000;
+int speciesOnEarth=8700000;
+long budgetOfKarnataka=1234456677;
+float freshWaterOnEarth=0.09f;
+double atomicMassOfBeryllium=9.012182d;
+char z=122;
+boolean sunIsAStar=true;
+System.out.println(digitInAClockDisplay);
+System.out.println(priceOfGoldPerGram);
+System.out.println(speciesOnEarth);
+System.out.println( budgetOfKarnataka);
+System.out.println(freshWaterOnEarth);
+System.out.println(atomicMassOfBeryllium);
+System.out.println(z);
+System.out.println(sunIsAStar);
+}
+}
