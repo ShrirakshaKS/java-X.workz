731170725a00f3e006ece629e8e6388f1ee3ff92
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-22 20:33:27 +0530

    Update States.java

--- States.java
+++ States.java
@@ -1,28 +1,29 @@
 public class States{		
-	public static void main(String[] divider){		
-	System.out.println("JVM invoke main method");
+public static void main(String[] names){		
+System.out.println("JVM invokes main method");
 
-
-System.out.println("exit from main method");
-	int size=divider.length;
-	System.out.println("Number of states here are: "+divider.length);
+int countOfStates=names.length;
+System.out.println("Count of states: "+countOfStates);
 	
-	System.out.println("State name @ index 0 "+divider[0]);
-	System.out.println("State name @ index 1 "+divider[1]);
-	System.out.println("State name @ index 2 "+divider[2]);
-	System.out.println("State name @ index 3 "+divider[3]);
-	System.out.println("State name @ index 4 "+divider[4]);
-System.out.println("State name @ index 5 "+divider[5]);
-System.out.println("State name @ index 6 "+divider[6]);
-System.out.println("State name @ index 7 "+divider[7]);
-System.out.println("State name @ index 8 "+divider[8]);
-System.out.println("State name @ index 9 "+divider[9]);
-System.out.println("State name @ index 10 "+divider[10]);
-System.out.println("State name @ index 11 "+divider[11]);
-System.out.println("State name @ index 12 "+divider[12]);
-System.out.println("State name @ index 13 "+divider[13]);
-System.out.println("State name @ index 14 "+divider[14]);
+System.out.println("State name at index 0 "+names[0]);
+System.out.println("State name at index 1 "+names[1]);
+System.out.println("State name at index 2 "+names[2]);
+System.out.println("State name at index 3 "+names[3]);
+System.out.println("State name at index 4 "+names[4]);
+System.out.println("State name at index 5 "+names[5]);
+System.out.println("State name at index 6 "+names[6]);
+System.out.println("State name at index 7 "+names[7]);
+System.out.println("State name at index 8 "+names[8]);
+System.out.println("State name at index 9 "+names[9]);
+System.out.println("State name at index 10 "+names[10]);
+System.out.println("State name at index 11 "+ names[11]);
+System.out.println("State name at index 12 "+names[12]);
+System.out.println("State name at index 13 "+names[13]);
+System.out.println("State name at index 14 "+names[14]);
+System.out.println("State name at index 15 "+names[15]);
+System.out.println("State name at index 16 "+names[16]);
+System.out.println("State name at index 17 "+names[17]);
 
-System.out.println("exit from main method");
+System.out.println("exit from main");
 }
 }
