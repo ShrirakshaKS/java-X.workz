0b72b5f91a0828becedef46334f6aac313404640
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-22 20:43:51 +0530

    Update Online.java

--- Online.java
+++ Online.java
@@ -2,10 +2,11 @@ public class Online{
 	public static void main(String[] trainees)
 	{
 		System.out.println("JVM invokes main method");
-		int size=trainees.length;
-		System.out.println("size of an array" + size);
+		int countOfTrainees=trainees.length;
+		System.out.println("size of an array" + countOfTrainees);
 
 	System.out.println("element at index 0"  + trainees[0]);
 	System.out.println("element at index 1"  + trainees[1]);
+	System.out.println("element at index 2"  + trainees[2]);
           }
 }
