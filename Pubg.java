ab4e468e16db0b1d7435f7704b6842375d80d699
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-23 22:49:00 +0530

    Add files via upload

---
+++ Pubg.java
@@ -0,0 +1,10 @@
+public class Pubg{
+public static void supply()
+{
+System.out.println(" supply in pub-g");
+}
+public static void enemiesahead()
+{
+System.out.println(" enemies are always ahead in pub-g");
+}
+}
---
+++ Toy.java
@@ -0,0 +1,10 @@
+public class Toy {
+public static void main(String[] game)
+{
+System.out.println("JVM invokes main");
+	Pubg.supply();
+	Pubg.enemiesahead();
+System.out.println(" exit from main");
+}
+
+}
