1e96c754778d4d19569e72b2a21de5a35f01258b
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-23 22:38:59 +0530

    Add files via upload

---
+++ Architect.java
@@ -0,0 +1,12 @@
+public class Arch{
+public static void design(String[] material)
+{
+int numberOfMaterials=material.length;
+System.out.println("count of materials: " + numberOfMaterials);
+
+System.out.println("element at index 0" + material[0]);
+System.out.println("element at index 1" + material[1]);
+System.out.println("element at index 2" + material[2]);
+System.out.println("element at index 3" + material[3]);
+}
+}
---
+++ Building.java
@@ -0,0 +1,8 @@
+public class Building{
+public static void main(String[] home)
+{
+System.out.println("JVM invokes main method");
+Architect.design(home);
+System.out.println("exit from main");
+}
+}
