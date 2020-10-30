94b7e5d0b2cbdbedc9da95578b5434a9f7db34e6
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-10-30 18:05:22 +0530

    Update Cloudy.java

--- Cloudy.java
+++ Cloudy.java
@@ -2,8 +2,10 @@ public class Cloudy{
 public static void main(String[] rain)
 {
 printMail();
-printProgrammingLanguage("Java"); printProgrammingLanguage("Python");
-printProgrammingLanguage("C"); printProgrammingLanguage("C++");
+printProgrammingLanguage("Java");
+printProgrammingLanguage("Python");
+printProgrammingLanguage("C"); 
+printProgrammingLanguage("C++");
 printProgrammingLanguage("C#");
 }
 public static void printMail()
