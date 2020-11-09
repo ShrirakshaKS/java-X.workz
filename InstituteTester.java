4106dee378eaa911bd31ed04835adc3d7a188349
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-11-09 21:30:47 +0530

    Add files via upload

---
+++ InstituteTester.java
@@ -0,0 +1,14 @@
+public class InstituteTester{
+
+
+public static void main(String[] photos)
+{
+
+System.out.println("invoked main, doing switch and enum");
+
+InstitueFinder.lowestAndBestUsingString("XWORKZ");
+
+System.out.println("EXIT :: main, doing switch and enum");
+}
+
+}
