34607a059e244cba81f4e9381e09d89344a2acd1
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-11-16 23:31:12 +0530

    Example

---
+++ AmazonSearch.java
@@ -0,0 +1,15 @@
+package Com.Xworkz.online;
+
+public class AmazonSearch {
+
+			public static void main(String[] args) {
+
+			System.out.println("jvm kareyuvudu main");
+
+			System.out.println("Total items to search" + Amazon.totalItems());
+			Amazon.search("Wallet");
+
+			System.out.println("main nirgamana");
+		}
+
+	}
